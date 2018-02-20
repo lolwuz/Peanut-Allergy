083e60f78497a65123254ff3f9fb3232b5ddc324
Marten Hoekstra <martenhoekstra2@gmail.com>
2018-02-20 13:21:54 +0100

    Fix

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,6 @@
 import * as states from './states';
+
+console.log("INITIATING GAME"); 
 const GAME = new Phaser.Game(800, 1000, Phaser.AUTO);
 
 Object.keys(states).forEach(state => GAME.state.add(state, states[state]));
